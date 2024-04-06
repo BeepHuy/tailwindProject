f8943c4c1419a58e47e408527c649084145c065a
Hoang Quang Huy <hoanghuy02062002@gmail.com>
2024-04-06 19:33:51 +0700

    responsive design table

--- javsscript/javascript.js
+++ javsscript/javascript.js
@@ -1,3 +1,125 @@
+//slide 3
+let currentIndex3 = 0;
+const slides3 = document.querySelectorAll(".slide3");
+
+function showSlide3() {
+  slides3.forEach((slide3, index3) => {
+    if (
+      index3 === currentIndex3 ||
+      index3 === (currentIndex3 + 1) % slides3.length ||
+      index3 === (currentIndex3 + 2) % slides3.length
+    ) {
+      slide3.style.display = "inline-block";
+    } else {
+      slide3.style.display = "none";
+    }
+  });
+}
+
+function prevSlide3() {
+  currentIndex3 =
+    currentIndex3 === 0
+      ? slides3.length - 1
+      : (currentIndex3 - 1 + slides3.length) % slides3.length;
+  showSlide3();
+}
+
+function nextSlide3() {
+  currentIndex3 = (currentIndex3 + 1) % slides3.length;
+  showSlide3();
+}
+
+showSlide3();
+
+// slide 2
+let currentIndex2 = 0;
+const slides2 = document.querySelectorAll(".slide2");
+
+function showSlide2() {
+  slides2.forEach((slide2, index2) => {
+    if (
+      index2 === currentIndex2 ||
+      index2 === (currentIndex2 + 1) % slides2.length ||
+      index2 === (currentIndex2 + 2) % slides2.length
+    ) {
+      slide2.style.display = "inline-block";
+    } else {
+      slide2.style.display = "none";
+    }
+  });
+}
+
+function prevSlide2() {
+  currentIndex2 =
+    currentIndex2 === 0
+      ? slides2.length - 1
+      : (currentIndex2 - 1 + slides2.length) % slides2.length;
+  showSlide2();
+}
+
+function nextSlide2() {
+  currentIndex2 = (currentIndex2 + 1) % slides2.length;
+  showSlide2();
+}
+
+showSlide2();
+// slide 1
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide() {
+  slides.forEach((slide, index) => {
+    if (
+      index === currentIndex ||
+      index === (currentIndex + 1) % slides.length
+    ) {
+      slide.style.display = "flex";
+    } else {
+      slide.style.display = "none";
+    }
+  });
+}
+
+function prevSlide() {
+  currentIndex =
+    currentIndex === 0
+      ? slides.length - 1
+      : (currentIndex - 1 + slides.length) % slides.length;
+  showSlide();
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide();
+}
+
+showSlide();
+//
+function initializeProgressBar(progress, prevBtn, nexBtn) {
+  let currentPosition = 0; // Vị trí ban đầu
+  const progressBar = document.querySelector(progress);
+
+  function updateProgressBar(delta) {
+    currentPosition += delta;
+    currentPosition = Math.max(0, Math.min(60, currentPosition)); // Giới hạn vị trí trong khoảng từ 0 đến 60
+    progressBar.style.left = currentPosition + "%";
+  }
+
+  document.querySelector(prevBtn).addEventListener("click", function () {
+    updateProgressBar(-30); // Di chuyển về phía trái 30%
+  });
+
+  document.querySelector(nexBtn).addEventListener("click", function () {
+    updateProgressBar(30); // Di chuyển về phía phải 30%
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeProgressBar("#loadA", "#prever", "#next");
+  initializeProgressBar("#loadA2", "#prever2", "#next2");
+  initializeProgressBar("#loadA3", "#prever3", "#next3");
+});
+// reponserve tablet
 const buyBtns = document.querySelectorAll(".js-by-bar");
 const modal = document.querySelector(".js-modal");
 const modalContainer = document.querySelector(".js-modal-container");
@@ -21,89 +143,21 @@ modalContainer.addEventListener("click", function (event) {
   event.stopPropagation();
 });
 
-function initializeProgressBar() {
-  let currentPosition = 0; // Vị trí ban đầu
-  const progressBar = document.querySelector("#loadA");
+//header modal
+// Lấy chiều cao của header2
+var header2Height = document.getElementById("header2").offsetHeight;
 
-  function updateProgressBar(delta) {
-    currentPosition += delta;
-    currentPosition = Math.max(0, Math.min(60, currentPosition)); // Giới hạn vị trí trong khoảng từ 0 đến 60
-    progressBar.style.left = currentPosition + "%";
+// Thêm sự kiện cho window khi cuộn trang
+window.addEventListener("scroll", function () {
+  // Lấy vị trí cuộn hiện tại của trang
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  // Nếu vị trí cuộn lớn hơn hoặc bằng chiều cao của header2
+  if (scrollPosition >= header2Height) {
+    // Hiển thị header1
+    document.getElementById("header1").classList.remove("hidden");
+  } else {
+    // Ẩn header1
+    document.getElementById("header1").classList.add("hidden");
   }
-
-  document.querySelector("#prever").addEventListener("click", function () {
-    updateProgressBar(-30); // Di chuyển về phía trái 30%
-  });
-
-  document.querySelector("#next").addEventListener("click", function () {
-    updateProgressBar(30); // Di chuyển về phía phải 30%
-  });
-}
-
-document.addEventListener("DOMContentLoaded", initializeProgressBar);
-
-function initialize() {
-  let current = 0; // Vị trí ban đầu
-  const progres = document.querySelector("#loadA2");
-
-  function updateProgres(delta) {
-    current += delta;
-    current = Math.max(0, Math.min(60, current)); // Giới hạn vị trí trong khoảng từ 0 đến 60
-    progres.style.left = current + "%";
-  }
-
-  document.querySelector("#prever2").addEventListener("click", function () {
-    updateProgres(-30); // Di chuyển về phía trái 30%
-  });
-
-  document.querySelector("#next2").addEventListener("click", function () {
-    updateProgres(30); // Di chuyển về phía phải 30%
-  });
-}
-
-document.addEventListener("DOMContentLoaded", initialize);
-
-function initiali() {
-  let curre = 0; // Vị trí ban đầu
-  const progre = document.querySelector("#load3");
-
-  function updatePro(delta) {
-    curre += delta;
-    curre = Math.max(0, Math.min(60, curre)); // Giới hạn vị trí trong khoảng từ 0 đến 60
-    progre.style.left = curre + "%";
-  }
-
-  document.querySelector("#prever3").addEventListener("click", function () {
-    updatePro(-30); // Di chuyển về phía trái 30%
-  });
-
-  document.querySelector("#next3").addEventListener("click", function () {
-    updatePro(30); // Di chuyển về phía phải 30%
-  });
-}
-
-document.addEventListener("DOMContentLoaded", initiali);
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   let currentPosition = 0; // Vị trí ban đầu
-
-//   const leftButton = document.querySelector("#prever");
-//   const rightButton = document.querySelector("#next");
-//   const progressBar = document.querySelector("#loadA");
-
-//   leftButton.addEventListener("click", function () {
-//     currentPosition -= 30; // 30% vị trí ở giữa
-//     currentPosition = Math.max(0, currentPosition);
-//     updateProgressBar();
-//   });
-
-//   rightButton.addEventListener("click", function () {
-//     currentPosition += 30; // 60% vị trí cuối
-//     currentPosition = Math.min(60, currentPosition);
-//     updateProgressBar();
-//   });
-
-//   function updateProgressBar() {
-//     progressBar.style.left = currentPosition + "%";
-//   }
-// });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   content: ["./**/*.html", "./**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     screens: {
-      xs: { max: "639px" },
-      sm: { min: "640px", max: "768px" },
+      x: { max: "480px" },
+      xs: { min: "481px", max: "639px" },
+      sm: { min: "642px", max: "768px" },
       md: { min: "769px", max: "1600px" },
     },
     extend: {
       boxShadow: {
+        boxH: "1px 2px 2px 2px #ffffff",
         box0: "1px 1px 10px 2px #e7dcdc",
         boxmobe: "0px 2px 0px 0px #cacfd2",
         boxmobehove: "0px 3px 4px 0px #f4a8a5",
@@ -23,9 +25,14 @@ module.exports = {
           from: { opacity: 0, transform: "translateX(300px)" },
           to: { opacity: 1, transform: "translateX(0)" },
         },
+        headerDrop: {
+          from: { opacity: 0, transform: "translateY(-120px)" },
+          to: { opacity: 1, transform: "translateY(0)" },
+        },
       },
       animation: {
         slowA: "formDrop ease 0.5s",
+        slowHD: "headerDrop ease 0.5s",
       },
     },
   },
